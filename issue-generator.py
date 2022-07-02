cc0187474ff7c3a0493cef1c1d2ce3317e2a01b2
Yeyao Liu <yliu.personal@gmail.com>
2022-07-02 13:59:50 +0100

    initial commit

---
+++ issue-generator.py
@@ -0,0 +1,17 @@
+import requests
+
+def main():
+    issueGenerator = IssueGenerator()
+
+
+class IssueGenerator:
+    def __init__(self):
+        self.token = 0
+        self.issues = 9
+        self.projects = 0
+        
+
+
+
+if __name__ == "__main__":
+    main()
